2c1a982f528878d90fd96c369e234cfd66dd9b72
eugene <onetwopro@yandex.ru>
2021-09-19 16:05:11 +0300

    Undefined fix

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -21,7 +21,7 @@ print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 # print("\nHouse sensor records for date {} = {}".format((date.today()).strftime("%m/%d/%y"), len(recs)))
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
-print("\nHouse sensor records for date {} = {}".format("02/19/21", len(recs)))
+print("\nHouse sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 # Module 3 code here:
 
 # Module 4 code here:
